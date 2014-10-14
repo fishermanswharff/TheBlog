513dab7c0c6355e906a9f5a49bc325595d5f8757
fishermanswharff <fishermanswharff@github.com>
2014-10-13 21:20:09 -0400

    implementing usernames attached to comment views. touching up some styling of forms

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,11 +1,18 @@
 class CommentsController < ApplicationController
   
-    
+  before_action :authenticate_user!, only: [:create,:destroy]
 
   def create 
     @article = Article.find(params[:article_id])
     @comment = @article.comments.create(comment_params)
+    @comment.user_id = current_user.id
+    @comment.save
+    @commenter = User.find(@comment.user_id)
+    @commenter = @commenter.email
     redirect_to article_path(@article)
+  end
+
+  def show
   end
 
   def destroy
@@ -17,7 +24,7 @@ class CommentsController < ApplicationController
 
   private 
   def comment_params
-    params.require(:comment).permit(:commenter, :body)
+    params.require(:comment).permit(:body, :user_id)
   end
 
 end
