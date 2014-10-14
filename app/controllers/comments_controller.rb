6a7bad8826a1dd6141f7c7d8d1697bc30fa0ed2c
fishermanswharff <fishermanswharff@github.com>
2014-10-14 12:51:43 -0400

    removing user_id from the permitted parameters

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -24,7 +24,7 @@ class CommentsController < ApplicationController
 
   private 
   def comment_params
-    params.require(:comment).permit(:body, :user_id)
+    params.require(:comment).permit(:body)
   end
 
 end
